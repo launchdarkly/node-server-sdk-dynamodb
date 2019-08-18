0beefa752905c35b4c5000b5e2962303b9a40c85
Eli Bishop <eli@launchdarkly.com>
2019-08-18 13:18:29 -0700

    update release metadata

--- .ldrelease/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-npm install
-npm run lint
-npm run check-typescript
-
-# Running the unit tests would require installing DynamoDB, which is slow. They've already been run in CI.
--- .ldrelease/update-version.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-PACKAGE_JSON_TEMP=./package.json.tmp
-sed "s/\"version\".*/\"version\": \"${LD_RELEASE_VERSION}\",/g" package.json > ${PACKAGE_JSON_TEMP}
-mv ${PACKAGE_JSON_TEMP} package.json
