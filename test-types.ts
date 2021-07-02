5ef0abfdfc14c816fec648422914a10c20162a9a
Eli Bishop <eli@launchdarkly.com>
2021-07-01 18:17:35 -0700

    fix TypeScript test code

--- test-types.ts
+++ test-types.ts
@@ -2,7 +2,7 @@
 // This file exists only so that we can run the TypeScript compiler in the CI build
 // to validate our index.d.ts file.
 
-import DynamoDBFeatureStore, { LDDynamoDBOptions } from 'launchdarkly-node-server-sdk-dynamodb';
+import { DynamoDBFeatureStore, LDDynamoDBOptions } from 'launchdarkly-node-server-sdk-dynamodb';
 import { LDLogger } from 'launchdarkly-node-server-sdk';
 import { DynamoDB } from 'aws-sdk';
 
