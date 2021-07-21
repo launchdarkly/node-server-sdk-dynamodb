76748101094a59f85c2422379909c4a5417a35d8
Eli Bishop <eli@launchdarkly.com>
2021-07-21 10:05:57 -0700

    fix exports

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const DynamoDBFeatureStore = require('./dynamodb_feature_store');
-const DynamoDBBigSegmentStore = require('./dynamodb_big_segment_store');
+const { DynamoDBBigSegmentStore } = require('./dynamodb_big_segment_store');
 
 module.exports = {
   DynamoDBFeatureStore,
