dc5d2766a707da33b91fe5349f73bcde9dd7b3b5
Eli Bishop <eli@launchdarkly.com>
2021-08-13 10:46:02 -0700

    Update index.d.ts
    Co-authored-by: ember-stevens <79482775+ember-stevens@users.noreply.github.com>

--- index.d.ts
+++ index.d.ts
@@ -15,7 +15,7 @@ declare module 'launchdarkly-node-server-sdk-dynamodb' {
    * Create a feature flag store backed by DynamoDB.
    *
    * @param tableName The table name in DynamoDB (required). The table must already exist.
-   *   See: https://docs.launchdarkly.com/sdk/features/storing-data/dynamodb#nodejs-server-side
+   *   See: https://docs.launchdarkly.com/sdk/features/storing-data/dynamodb
    * @param options Additional options for configuring the DynamoDB store's behavior.
    */
   export function DynamoDBFeatureStore(
